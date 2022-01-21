c464725a8b853999f867fcca05b2b3b24a979273
hubnotch <love2ekene@gmail.com>
2022-01-21 23:14:44 +0100

    figured out how to update the quiz questions

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,19 +1,20 @@
-import React from 'react'
+import React from "react";
 // import Button from "./Button.js";
 
 function Question(props) {
-  console.log(props.incorrectAnswers)
-    return (
-      <div className="question">
-        <h3>{props.question}</h3>
-        <div className="question--options">
-          <button className="btn options">{props.incorrectAnswers}</button>
-          {/* <button className="btn options">Hola</button>
-          <button className="btn options">Au Revoir</button>
-          <button className="btn options">Salir</button> */}
-        </div>
+  console.log(props);
+  // console.log(props.correctAnswer)
+  return (
+    <div className="question">
+      <h3>{props.question}</h3>
+      <div className="question--options">
+        <button className="btn options">{props.correct_answer}</button>
+        <button className="btn options">{props.incorrect_answers[0]}</button>
+        <button className="btn options">{props.incorrect_answers[1]}</button>
+        <button className="btn options">{props.incorrect_answers[2]}</button>
       </div>
-    );
+    </div>
+  );
 }
 
-export default Question
+export default Question;
--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,15 +1,22 @@
 import React from 'react'
 import Question from './Question.js'
 import data from "../data.js"
+// import {nanoid} from "nanoid"
 
 function Quiz() {
+
     const questionsArray = data["results"]
-    console.log(questionsArray[0]);
+    
+    //console.log(questionsArray[0]);
+    
     const questions = questionsArray.map((question) => (
-    <Question key={question.question} 
-        question={question.question} 
-        correctAnswer = {question.correct_answer}
-        incorrectAnwers = {question.incorrect_answers.map(option=> option)}
+    
+    <Question key= {question} 
+
+    {...question}
+        // question={question} 
+        // correctAnswer = {question.correct_answer}
+        // incorrectAnwers = {question.incorrect_answers.map(answer=>answer)}
     />
     ));
     return (
